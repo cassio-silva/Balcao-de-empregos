2b88fc1d3472ac59a6780ada8a1dd3a7975f3187
Cassio Silva <cassioglopesdasilva@hotmail.com>
2018-01-15 18:26:03 -0200

    Commit teste

--- View/modeloView.php
+++ View/modeloView.php
@@ -1,12 +1,23 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="pt-br" xmlns="http://www.w3.org/1999/xhtml">
+    <title>Página Teste</title>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="Bootstrap/css/bootstrap.css" type="text/css" charset="utf-8"/>
+        <link rel="stylesheet" href="Bootstrap/css/bootstrap-theme.css" type="text/css" charset="UTF-8"/>
+        <link rel="stylesheet" href="Bootstrap/css/bootstrap-theme.min.css" type="text/css" charset="utf-8"/>
+        <link rel="stylesheet" href="Bootstrap/css/custom-style.css" type="text/css" charset="utf-8"/>
+
     </head>
     <body>
         <?php
-        // put your code here
+        
+        
         ?>
+        <!-- ******************************* SCRIPTS ******************************** -->
+        <script src="Bootstrap/js/jquery.js" type="text/javascript" charset="utf-8"></script>
+        <script src="Bootstrap/js/bootstrap.js" type="text/javascript" charset="utf-8"></script>
+        <script src="Bootstrap/js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
     </body>
 </html>
